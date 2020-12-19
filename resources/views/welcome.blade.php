f07be8b3d95f34fccd26ee7f5dde7c9ce2b449cd
Gastón Estévez <gastonestevez@MacBook-Air-de-Gaston.local>
2020-12-19 15:33:35 -0300

    first commit - landing

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ecsa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="/app.css">
+    <script src="https://kit.fontawesome.com/c9d2de6379.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="header__box">
+                <h2 class="header__box__title">E.C.S.A.</h2>
+                <p class="header__box__p">Control, saneamiento ambiental, fumigaciones y limpieza de tanques</p>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="servicios__box container">
+            <h3 class="serviciosTitle">Servicios</h3>
+            <p class="serviceText">
+                Tenemos más de 15 años de experiencia en fumigaciones, control de plagas, desratización, y Limpieza de tanques.
+                
+                Servicios para Particulares, Comercios, Consorcios, Empresas e Industrias (destacar esto en algun lado)
+                
+                En ECSA trabajamos de manera conjunta con el cliente a fin de determinar de manera correcta y efectiva el mejor método para la solución de sus necesidades, buscando siempre proteger la salud de las personas y nuestro medio ambiente. Trabajamos cada día para solidificar nuestra posición y ser el socio preferido de las empresas en todos los mercados que servimos, al participar y cubrir cada una de las necesidades de nuestros clientes.
+                 </p>
+        <div class="cards">
+            <div class="card" style="width: 18rem;">
+                <img src="/storage/img/s2.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Fumigación</h5>
+                    <p class="card-text">Con productos inofensivos en personas y mascotas,  nuestro equipo dejará su casa y jardín libre de insectos en pocas horas.</p>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+                <img src="/storage/img/s1.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Desratización</h5>
+                  <p class="card-text">Realizamos una desinfección y posterior limpieza de todas las superficies para eliminar cualquier rastro biológico que pueda permanecer en ella.</p>
+                </div>
+              </div>
+              <div class="card" style="width: 18rem;">
+                <img src="/storage/img/s3.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Limpieza de tanques</h5>
+                  <p class="card-text">Realizamos la limpieza y desinfección, realizando el análisis bacteriológico, garantizando la seguridad y potabilidad del agua, según legislación vigente Ley 6040/18 Dec405/18.</p>
+                </div>
+              </div>
+        </div>
+        
+        </div>
+
+    </main>
+    <footer>
+        <div class='footer container'>
+            <div class="contactItem">
+                <div class="title-box">
+                    <i class="far fa-map icon"></i>
+                    <h5 class="title">Dirección</h5>
+                </div>
+                <p class="text">Llavallol 2987 CABA</p>
+            </div>
+            <div class="contactItem">
+                <div class="title-box">
+                    <i class="fab fa-whatsapp icon"></i>
+                    <h5 class="title">Whatsapp</h5>
+                </div>
+                <p class="text">11 6134 9283</p>
+            </div>
+            <div class="contactItem">
+                <div class="title-box">
+                    <i class="far fa-envelope icon"></i>
+                    <h5 class="title">Mail</h5>
+
+                </div>
+                <p class="text">ecsafumigaciones@hotmail.com</p>
+            </div>
+        </div>
+        <div class="develop__box">
+            <p class="develop"> 
+                <a target='_blank' href="https://www.dakotadevelopers.com/">
+                Desarrollado por Dakota Developers
+                </a> 
+            </p>
+            <i class="far fa-copyright icon"></i>
+        </div>
+    </footer>
+    <div class="w__button">
+        <a href="https://api.whatsapp.com/send?phone=1161349283">
+            <img src="/storage/img/w.svg" alt="whatsapp" srcset="">
+        </a>
+    </div>
+    <script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    </script>
+</body>
+</html>
